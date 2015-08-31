de2e6ab208e28a2dbe5c48143c8f39f414df198b
VKarpenko <Valeriia.Karpenko@billing.ru>
2015-08-31 11:48:13 +0300

    ДЗ1
    Работа по:
    ДЗ1: Выполнить действия, продемонстрированные в классе

---
+++ address-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -0,0 +1,29 @@
+package com.example.tests;
+
+import org.testng.annotations.Test;
+
+public class GroupCreationTests extends TestBase{
+  @Test
+  public void testNonEmptyGroupCreation() throws Exception {
+	openManePage();
+	gotoGroupsPage();
+    initGroupCreation();
+    GroupData group = new GroupData("group 1", "header 1", "footer 1");
+	group.name = "group name 1";
+    group.header = "header 1";
+	group.footer = "footer 1";			
+    fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+  
+  @Test
+  public void testEmptyGroupCreation() throws Exception {
+	openManePage();
+	gotoGroupsPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("", "", ""));
+    submitGroupCreation();
+    returnToGroupsPage();
+  }
+}
