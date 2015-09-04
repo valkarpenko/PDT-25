376a8387ffd8d457712575a8d0c37375610567db
VKarpenko <Valeriia.Karpenko@billing.ru>
2015-09-04 10:45:00 +0300

    ДЗ3: Выполнить действия, продемонстрированные в классе

--- address-selenium-tests/src/com/example/tests/AlsoGroupCreation.java
+++ address-selenium-tests/src/com/example/tests/AlsoGroupCreation.java
@@ -6,26 +6,26 @@ public class AlsoGroupCreation extends TestBase{
 
   @Test
   public void testNonEmptyGroupCreation() throws Exception {
-	openMainPage();
-	gotoGroupsPage();
-    initGroupCreation();
+	app.getNavigationHelper().openMainPage();
+	app.getNavigationHelper().gotoGroupsPage();
+    app.getGroupHelper().initGroupCreation();
     GroupData group = new GroupData("group 1", "header 1", "footer 1");
 	group.name = "group name 1";
     group.header = "header 1";
 	group.footer = "footer 1";			
-    fillGroupForm(group);
-    submitGroupCreation();
-    returnToGroupsPage();
+    app.getGroupHelper().fillGroupForm(group);
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupsPage();
   }
   
   @Test
   public void testEmptyGroupCreation() throws Exception {
-	openMainPage();
-	gotoGroupsPage();
-    initGroupCreation();
-    fillGroupForm(new GroupData("", "", ""));
-    submitGroupCreation();
-    returnToGroupsPage();
+	app.getNavigationHelper().openMainPage();
+	app.getNavigationHelper().gotoGroupsPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("", "", ""));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupsPage();
   }
 
 }
--- address-selenium-tests/src/com/example/tests/ContactCreation.java
+++ address-selenium-tests/src/com/example/tests/ContactCreation.java
@@ -6,12 +6,10 @@ public class ContactCreation extends TestBase{
 
   @Test
   public void testNonEmptyContactCreation() throws Exception {
-	openMainPage();
-	gotoContactPage();
-    fillContactForm(new ContactData("Filippov", "Pavel", "SPb", "5656666", "79115856699", "8888888", "filippov@ya.ru", "filippov2@ya.ru", "21", "February", "1985", "Saint-Petersburg", "Moscow", "9999999"));
-    submitContactCreation();
+	app.getNavigationHelper().openMainPage();
+	app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("Filippov", "Pavel", "SPb", "5656666", "79115856699", "8888888", "filippov@ya.ru", "filippov2@ya.ru", "21", "February", "1985", "Saint-Petersburg", "Moscow", "9999999"));
+    app.getContactHelper().submitContactCreation();
   }
-
-
 
 }
